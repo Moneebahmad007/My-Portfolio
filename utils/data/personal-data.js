2cf344581e963c9790fc74f2b993913da2abf6d4
Moneebahmad007 <165939846+Moneebahmad007@users.noreply.github.com>
2024-04-04 00:20:33 +0500

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Moneeb Ahmad Amin",
-  profile: '\me.png',
+  profile: 'me.png',
   designation: "Software Engineer",
   description: "I am a graduate from the esteemed University of Lahore with a degree in Software Engineering, eager to embark on a fulfilling journey in the tech industry. With a solid foundation in React Native, honed through my Final Year Project, and proficiency in Web Development, I am excited to leverage my skills and contribute to innovative projects. Additionally, I possess initial level experience in Python and am committed to continuous learning and growth in this dynamic field.",
   email: 'moneebahmad51@gmail.com',
